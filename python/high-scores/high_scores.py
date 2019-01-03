c49db5659155cc06995f575e4dbc4fd088b71f48
Mark Chesney <mark.chesney@gmail.com>
2019-01-02 22:28:13 -0800

    prepare high_scores.py for unit testing

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -12,16 +12,23 @@ Your task is to write methods that return
 
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        self.scores = scores
 
-    def personal_best():
-        pass
+    def personal_best(scores):
+        return max(scores)
 
-    def latest():
-        pass
+    def latest(scores):
+        return scores[-1]
 
-    def personal_top():
-        pass
+    def personal_top(scores):
+        temp_list = scores.copy()
+        temp_list.sort(reverse=True)
+        return temp_list[:3]
 
-    def report():
-        pass
+    def report(scores):
+        latest = latest(scores)
+        personal_best = personal_best(scores)
+        if latest == personal_best:
+            message = "Your latest score was {0}. That's your personal best!".format(latest)
+        else:
+            message = "Your latest score was {0}. That's {1} short of your personal best!".format(latest, personal_best-latest)
